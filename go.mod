27a4ac7563441acdbd804faa5dcc766d08c82a04
kdimtri <kurunov.dmitry@gmail.com>
2021-08-18 10:42:00 +0400

    working version

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/kdimtri/sFConv
 
 go 1.16
 
-require github.com/prometheus/common v0.30.0 // indirect
+require (
+	github.com/go-yaml/yaml v2.1.0+incompatible // indirect
+	github.com/prometheus/client_model v0.2.0
+	github.com/prometheus/common v0.30.0
+	gopkg.in/yaml.v2 v2.4.0
+)
--- main.go
+++ main.go
@@ -5,36 +5,61 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/go-yaml/yaml"
+	dto "github.com/prometheus/client_model/go"
 	"github.com/prometheus/common/expfmt"
 )
 
-const filepath = "./in.yaml"
-const format = "text/yaml"
+const (
+	file = "in.yaml"        // input file
+	addr = "localhost:8080" // server address
+)
+
+var metrics *dto.MetricFamily
 
 func main() {
-	http.Handle("/metrics", http.HandlerFunc(openMetricsResponse))
-	log.Println("localhost:8080")
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	metrics = new(dto.MetricFamily)
+	if err := readInput(file, metrics); err != nil {
+		log.Fatalf("Input data  erro: %v", err)
+	}
+	http.Handle("/metrics", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if err := readInput(file, metrics); err != nil {
+			fmt.Fprintln(w, "Input data reading error:", err)
+		} else {
+			enc := expfmt.NewEncoder(w, expfmt.FmtOpenMetrics)
+			if err := enc.Encode(metrics); err != nil {
+				fmt.Fprintln(w, "Input data encoding error:", err)
+			}
+		}
+	}))
+	log.Println("Listening on", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
-func openMetricsResponse(w http.ResponseWriter, r *http.Request) {
 
-	log.Println(r.Header, r.RequestURI, r.URL)
-	b, err := os.Open(filepath)
+var readTime, modTime time.Time
+
+func readInput(file string, metrics *dto.MetricFamily) error {
+	stat, err := os.Lstat(file)
 	if err != nil {
-		fmt.Fprintf(w, "Open file: %v, %v", filepath, err)
-		log.Println("file at ", filepath, ": ", err)
+		return fmt.Errorf("Reading file info: %q error:\n%v", file, err)
 	}
-	defer b.Close()
-	log.Println("file at ", filepath, ": ok")
-	encoder := expfmt.NewEncoder(w, expfmt.FmtOpenMetrics)
-	parser := expfmt.TextParser{}
-	m, err := parser.TextToMetricFamilies(b)
-	if err != nil {
-		fmt.Fprintf(w, "parser.TextToMetricFamilies: %v", err)
-		log.Printf("parser.TextToMetricFamilies: %v", err)
+	if stat.ModTime().Equal(modTime) == false || readTime.IsZero() || stat.ModTime().After(readTime) {
+		r, err := os.Open(file)
+		if err != nil {
+			return fmt.Errorf("Opening file: %q error:\n%v", file, err)
+		}
+		defer r.Close()
+		modTime = stat.ModTime()
+		readTime = time.Now()
+		dec := yaml.NewDecoder(r)
+		if err := dec.Decode(metrics); err != nil /* && err != io.EOF */ {
+			return fmt.Errorf("Error decoding File: %q\n: %v\n", file, err)
+		}
+		log.Printf("File :%q has being read  at[ %v ]", file, readTime)
+	} else {
+		log.Printf("File :%q has't change since last time reading [ %v ]", file, readTime)
 	}
-	for _, v := range m {
-		encoder.Encode(v)
-	}
+	return nil
 }
